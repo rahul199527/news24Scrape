cc030a79b5dd0e173646b98ed3271464d54de0b7
Rahul Kadri <rahul.kadri@techfinium.com>
2021-05-27 13:18:32 +0100

    functionwrapped

--- news24.py
+++ news24.py
@@ -1,100 +1,88 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from parsel import Selector
-from bs4 import BeautifulSoup
-import random
-import re
-import csv
-import urllib
-import requests
+def news24():
+    import time
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    from parsel import Selector
+    from bs4 import BeautifulSoup
+    import random
+    import re
+    import csv
+    import urllib
+    import requests
 
 
-company = input("Enter company name :" )  ## has to taken from user input
+    company = input("Enter company name :" )  ## has to taken from user input
 
-driver = webdriver.Chrome(executable_path=r'C:\\Python\\news24\\chromedriver.exe')
-driver.get('https://www.news24.com/')
-driver.maximize_window()
+    driver = webdriver.Chrome(executable_path=r'C:\\Python\\news24\\chromedriver.exe')
+    driver.get('https://www.news24.com/')
+    driver.maximize_window()
 
-driver.implicitly_wait(10)
+    driver.implicitly_wait(10)
 
-#url = urllib.urlopen('https://www.news24.com/')
-#content = url.read()
-content = driver.page_source
-soup = BeautifulSoup(content, 'html.parser')
-H = driver.execute_script("return document.documentElement.scrollHeight")
-#print(H)
+    ## passing name of the company to search
 
-#time.sleep(5)
-#n = range(0, 14000, 1000)
-
-#driver.execute_script("window.scrollBy(0,'n')")
-#time.sleep(10)
-
-inputElement = driver.find_element_by_id("search")
-inputElement.send_keys(company)
-inputElement.send_keys(Keys.ENTER)
+    inputElement = driver.find_element_by_id("search")
+    inputElement.send_keys(company)
+    inputElement.send_keys(Keys.ENTER)
 
 
-
-time.sleep(5)
-driver.execute_script("window.scrollBy(0,1000)")
-time.sleep(5)
-driver.execute_script("window.scrollBy(0,1000)")
-time.sleep(10)
-driver.execute_script("window.scrollBy(0,1000)")
-time.sleep(10)
-driver.execute_script("window.scrollBy(0,1000)")
-time.sleep(10)
-driver.execute_script("window.scrollBy(0,1000)")
-time.sleep(10)
-driver.execute_script("window.scrollBy(0,1000)")
-ime.sleep(10)
-
-
-a_elements = []
-matches = [] ## contains the filtered list of companies
-t_element = [] ## This is the list with all the title
-p_element = []
-
-content_blocks = driver.find_elements_by_class_name("article-list--container")
-
-for block in content_blocks:
-    elements = block.find_elements_by_tag_name("a")
-    #elements.get_Text()
-    for el in elements:
-        a_elements.append(el.get_attribute("href"))      
-
-for match in a_elements:
-            if company in match:
-                matches.append(match)
+    ## basic scroll needs to be improved
+    time.sleep(5)
+    driver.execute_script("window.scrollBy(0,1000)")
+    time.sleep(5)
+    driver.execute_script("window.scrollBy(0,1000)")
+    time.sleep(10)
+    driver.execute_script("window.scrollBy(0,1000)")
+    time.sleep(10)
+    driver.execute_script("window.scrollBy(0,1000)")
+    time.sleep(10)
+    driver.execute_script("window.scrollBy(0,1000)")
+    time.sleep(10)
+    driver.execute_script("window.scrollBy(0,1000)")
+    ime.sleep(10)
 
 
-## number of articles collected
-print (len(matches))
+    a_elements = [] 
+    ## contains the filtered list of companies
+    matches = [] 
+    ## This is the list with all the title
+    t_element = [] 
+    p_element = []
+
+    content_blocks = driver.find_elements_by_class_name("article-list--container")
+
+    for block in content_blocks:
+        elements = block.find_elements_by_tag_name("a")
+        #elements.get_Text()
+        for el in elements:
+            a_elements.append(el.get_attribute("href"))      
+
+    for match in a_elements:
+                if company in match:
+                    matches.append(match)
 
 
-## scraping title and body of article, storing to a list of dictionaries
-sample = {'title': [], 'article': []}
-result = []
-for j in matches:
-    try: 
-        driver.get(j)
-        titleElement = driver.find_element_by_tag_name('h1').text
-        bodyElement = driver.find_element_by_class_name('article__body').get_attribute("textContent")
-        #sample.update({'title' : titleElement})
-        #sample.update({'article' : bodyElement})
-        sample = {'title': titleElement, 'article': bodyElement}
-        #sample["title"].update(titleElement)
-        #sample["article"].append(bodyElement)
-        result.append(sample)
-        #print(titleElement)
-        #print('\n')
-        #print(bodyElement)
-    except:
-        print("Needs a Subscriber Account")
+    ## number of articles collected
+    print (len(matches))
 
-#for pair in sample.items():
- #   print(pair)
 
-#print(result)
+    ## scraping title and body of article, storing to a list of dictionaries
+    sample = {'title': [], 'article': []}
+    result = []
+    for j in matches:
+        try: 
+            driver.get(j)
+            titleElement = driver.find_element_by_tag_name('h1').text
+            bodyElement = driver.find_element_by_class_name('article__body').get_attribute("textContent")
+            #sample.update({'title' : titleElement})
+            #sample.update({'article' : bodyElement})
+            sample = {'title': titleElement, 'article': bodyElement}
+            result.append(sample)
+            
+        except:
+            print("Needs a Subscriber Account")
+
+    #print(result)
+
+if __name__ == "__main__":
+    news24()
